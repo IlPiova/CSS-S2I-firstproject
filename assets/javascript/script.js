6d6289d846f8eed096d6cc34ff6633d423569f8d
Cristian <piovani.cp@gmail.com>
2024-04-21 13:26:19 +0200

    v1.2.3
    corretto invio mail senza campi obbligatori

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,24 @@
 function sendMail() {
-    let params = {
-        from_name: document.getElementById("name").value,
-        surname: document.getElementById("surname").value,
-        from_mail: document.getElementById("from_mail").value,
-        message: document.getElementById("message").value
+    let name = document.getElementById("name").value;
+    let lastname = document.getElementById("lastname").value;
+    let from_mail = document.getElementById("from_mail").value;
+    let message = document.getElementById("message").value;
+
+    if (name && lastname && from_mail && message) {
+        let params = {
+            from_name: name,
+            lastname: lastname,
+            from_mail: from_mail,
+            message: message
+        };
+
+        emailjs.send("service_t7921jo", "template_3bj8hr8", params)
+            .then((response) => {
+                alert('Email inviata con successo!', response.status, response.text);
+            }, (err) => {
+                alert('Invio email fallito...', err);
+            });
+    } else {
+        alert('Per favore compila tutti i campi obbligatori.');
     }
-    emailjs.send("service_t7921jo", "template_3bj8hr8", params)
-        .then((response) => {
-            alert('SUCCESS!', response.status, response.text);
-        }, (err) => {
-            alert('FAILED...', err);
-        });
 }
